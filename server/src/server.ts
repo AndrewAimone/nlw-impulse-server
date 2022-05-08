a42ab95a12b5091eac21e9090717990449f7a323
AndrewAimone <andrewaimone.info@gmail.com>
2022-05-07 22:22:35 -0300

    large json

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,12 +4,16 @@ import cors from 'cors'
 
 const app = express()
 
-
 app.use(cors())
-app.use(express.json())
+app.use(express.json({ limit: '10mb' }))
+app.use(
+  express.urlencoded({
+    limit: '10mb',
+    extended: true,
+    parameterLimit: 100000
+  })
+)
 app.use(routes)
-
-
 app.listen(process.env.PORT || 3333, () =>{
     console.log('HTTP Server Running!')
 })
